04689006a572f539cfdb852e6651569349f3c8c1
Clement Perier Muzet <barceloneta08003@gmail.com>
2020-10-19 01:37:51 +0200

    styling half done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,12 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:show, :edit, :update, :destroy]
   
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
   end
 
   def new
@@ -17,22 +18,34 @@ class ArticlesController < ApplicationController
   @article.save
 
   redirect_to article_path(@article)
-end
+  end
 
-def edit
-  @article = Article.find(params[:id])
-end
+  def edit
+  # @article = Article.find(params[:id])
+  end
 
-def update
-  @article = Article.find(params[:id])
-  @article.update(article_params)
-  redirect_to article_path(@article)
-end
+  def update
+    # @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+  # @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path(@article)
+  end
 
 
   private
 
   def article_params
-    params.require(:article).permit(:name, :content)
+    params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
+
+    
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
 
   get 'articles/:id/edit', to: 'articles#edit'
   patch 'articles/:id', to: 'articles#update'
+
+  delete 'articles/:id', to: 'articles#destroy'
 end
